8edd84f637e7d75706571761a86baee66f6872fd
authorunknown <authorunknown@gmail.com>
2010-12-01 19:09:23 -0500

    added mvc html helper method for outputting javascript

---
+++ json-util/src/ConsoleApplication1/Js.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+public class Js
+{
+    string _value;
+
+    private Js(string value)
+    {
+        _value = value;
+    }
+
+    public override string ToString()
+    {
+        return _value;
+    }
+
+    public static Js Variable(string name, object value)
+    {
+        return new Js(string.Format("{0} = {1};", name, JsonConvert.SerializeObject(value)));
+    }
+}
+
+public static class HtmlHelperExtensions
+{
+    public static MvcHtmlString ScriptBlock(this HtmlHelper html, params Js[] content)
+    {
+        if (content == null || content.Length == 0)
+            return MvcHtmlString.Empty;
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("<script type=\"text/javascript\">");
+        sb.AppendLine("//<![CDATA[");
+        foreach (Js v in content)
+            sb.AppendLine(v.ToString());
+        sb.AppendLine("//]]>");
+        sb.AppendLine("</script>");
+        return MvcHtmlString.Create(sb.ToString());
+    }
+}
---
+++ json-util/src/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace ConsoleApplication1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Html.ScriptBlock(
+                Js.Variable("x", 1),
+                Js.Variable("y", new { name = "foo", value = "bar" }));
+            Console.WriteLine(result.ToString());
+        }
+
+        private static HtmlHelper Html;
+    }
+}
